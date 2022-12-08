927e9f83af70b6bef10dfbe0a499e2423ad4dd76
unknown <Prakashe121100@gmail.com>
2022-12-08 11:29:17 +0530

    done

--- prime.js
+++ prime.js
@@ -1,7 +1,7 @@
 let num = 14;
 let count = 0;
-for(let i = 1; i=num; i++){
-if(number %i== 1){
+for(let i = 1; i<=num; i++){
+if(num %i== 0){
 count++;
 }
 }
@@ -9,5 +9,22 @@ if(count == 2){
 console.log("prime number")
 }
 else{
-console.log();
+console.log("Not a Prime number");
 }
+
+// code 
+
+ let num=14;
+let c =0;
+for(i=1;i<=num;i++){
+if (num%i==0){
+ c++;
+}
+}
+if(c==2){
+console.log("true")
+}
+else{
+console.log("false)
+}
+
